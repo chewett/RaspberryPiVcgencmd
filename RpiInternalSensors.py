54724b8df7d41a2b50f297f62bb5ff67faa83302
Christopher Hewett <chewett@hotmail.co.uk>
2017-05-27 18:17:23 +0000

    Added clock voltages to the library

--- RpiInternalSensors.py
+++ RpiInternalSensors.py
@@ -14,14 +14,22 @@ class RpiInternalSensors:
     def get_ram_split(self):
         lines_arm = subprocess.check_output(["vcgencmd", "get_mem", "arm"])
         lines_gpu = subprocess.check_output(["vcgencmd", "get_mem", "gpu"])
-        arm = self.parse_lines(lines_arm)['arm']
-        gpu = self.parse_lines(lines_gpu)['gpu']
+        arm = self._parse_lines(lines_arm)['arm']
+        gpu = self._parse_lines(lines_gpu)['gpu']
         return {
             "arm": arm,
             "gpu": gpu
         }
 
-    def parse_lines(self, lines):
+    def measure_volts(self, type="core"):
+        if type in ["core", "sdram_c", "sdram_i", "sdram_p"]:
+            lines = subprocess.check_output(["vcgencmd", "measure_volts", type])
+            data = self._parse_lines(lines)
+            return float(data['volt'][:-1])
+        else:
+            raise ValueError("type must be one of core, sdram_c, sdram_i, sdram_p")
+
+    def _parse_lines(self, lines):
         split_lines = lines.split("\n")
         dict_response = {}
         for line in split_lines:
--- print_all_sensors.py
+++ print_all_sensors.py
@@ -4,4 +4,8 @@ sen = RpiInternalSensors()
 
 print sen.get_cpu_temp(), "'C"
 print sen.get_cpu_temp(fahrenheit=True), "'F"
-print sen.get_ram_split()
+print "Ram split", sen.get_ram_split()
+print "Core voltage", sen.measure_volts()
+print "sdram_c voltage", sen.measure_volts("sdram_c")
+print "sdram_i voltage", sen.measure_volts("sdram_i")
+print "sdram_p voltage", sen.measure_volts("sdram_p")
