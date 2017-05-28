a3b5668272fb3068081e67850af184bf42f715d0
Christopher Hewett <chewett@hotmail.co.uk>
2017-05-28 15:44:44 +0100

    moved around the test functions a little

--- print_all_sensors.py
+++ print_all_sensors.py
@@ -11,12 +11,12 @@ voltages_to_test = ["core", "sdram_c", "sdram_i", "sdram_p"]
 for voltage in voltages_to_test:
     print voltage, "voltage", sen.measure_volts(voltage)
 
-codecs_to_test = ["H264", "MPG2", "WVC1", "MPG4", "MJPG", "WMV9"]
-for codec in codecs_to_test:
-    print codec, "available", sen.is_codec_available(codec)
-
 clocks_to_measure = ["arm", "core", "h264", "isp", "v3d", "uart", "pwm", "emmc", "pixel", "vec", "hdmi", "dpi"]
 for clock in clocks_to_measure:
     print clock, "speed", sen.measure_clock(clock)
+
+codecs_to_test = ["H264", "MPG2", "WVC1", "MPG4", "MJPG", "WMV9"]
+for codec in codecs_to_test:
+    print codec, "available", sen.is_codec_available(codec)
 
 print "Version: ", sen.get_version()
