0596270a1362c90873747a7e8bc1026974095233
Christopher Hewett <chewett@hotmail.co.uk>
2021-01-03 14:07:17 +0000

    Move to v0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='RaspberryPiVcgencmd',
-    version='0.1',
+    version='0.2',
     packages=['RaspberryPiVcgencmd'],
     url='https://github.com/chewett/RaspberryPiVcgencmd',
     license='MIT',
