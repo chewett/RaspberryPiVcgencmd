9ff44b5f9355c41efd94edd39473d7e9669c0780
Christopher Hewett <chewett@hotmail.co.uk>
2021-01-08 18:17:32 +0000

    Make this be imported

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='RaspberryPiVcgencmd',
-    version='0.2',
+    version='0.2.1',
     packages=['RaspberryPiVcgencmd'],
     url='https://github.com/chewett/RaspberryPiVcgencmd',
     license='MIT',
