0aadcba3e9b51ea927ba1f679065fbc8dd5e6af5
Christopher Hewett <chewett@hotmail.co.uk>
2021-01-09 01:02:29 +0000

    Bump version number to match docs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='RaspberryPiVcgencmd',
-    version='1.0',
+    version='1.0.1',
     packages=['RaspberryPiVcgencmd'],
     url='https://github.com/chewett/RaspberryPiVcgencmd',
     license='MIT',
